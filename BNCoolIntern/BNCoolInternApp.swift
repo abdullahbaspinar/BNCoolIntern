6112c254c0a9e2ec9d1b7e846fa0bd3eeaae5f76
abaspnr <117168519+abaspnr@users.noreply.github.com>
2025-08-13 23:23:11 +0300

    Initial Commit

---
+++ BNCoolIntern/BNCoolInternApp.swift
@@ -0,0 +1,17 @@
+//
+//  BNCoolInternApp.swift
+//  BNCoolIntern
+//
+//  Created by Abdullah B on 13.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct BNCoolInternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
